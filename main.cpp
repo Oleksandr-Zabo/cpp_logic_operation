d09f2e1d13abd84607e6d0d11cbeebcd137d908a
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-25 19:54:27 +0300

    ex. 2 hw had done

--- main.cpp
+++ main.cpp
@@ -4,8 +4,16 @@ using namespace std;
 int main()
 {
 	float num_in;
+	int hryvnias, kopecks;
+	cout << "Enter your money: ";
+	cin >> num_in;
+	int num_in2 =num_in * 100;
+	hryvnias = num_in2 / 100;
+	kopecks = num_in2 %100;
 
-	cout << "Hello world(dev)"<<endl;
+
+	cout << "You have: " << hryvnias << " grn and " << kopecks << " kop" << endl;
+
 	system("pause");
 	return 0;
 }
