05e6bb7c2f75ab4956831201411168cdfe25da8f
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-25 20:23:24 +0300

    add M_PI

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <cmath>
+#define _USE_MATH_DEFINES
+#include <math.h>
 using namespace std;
 
 int main()
 {
-	const double PI = 3.141592;
 	double radius, volume;
 
 	cout << "Calculation of the volume of a sphere\n"
 		<<"Enter the radius (cm): ";
 	cin >> radius;
-	volume = (4.0 / 3.0) * PI * pow(radius, 3);
+	volume = (4.0 / 3.0) * M_PI * pow(radius, 3);
 	cout << "Volume of the sphere: "<< volume<<" cm cubic" << endl;
 	system("pause");
 	return 0;
