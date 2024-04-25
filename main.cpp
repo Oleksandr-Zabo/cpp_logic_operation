0e3878ca1e7cfaa008ad3bfb3d4380973e456a7f
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-25 19:45:54 +0300

    ex. 1 hw had done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello world(dev)"<<endl;
+	int sec_in, hours, min, sec;
+	cout << "Time conversion from seconds\n"
+		<<"Enter time(in seconds: ";
+
+	cin >> sec_in;
+	hours=sec_in/3600;
+	min = (sec_in / 3600) / 60;
+	sec = (sec_in/3600)%60;
+
+	cout << "Hours: "<<hours<<endl
+		<< "Minutes: " << hours<<endl
+		<< "Seconds: " << sec << endl;
 	system("pause");
 	return 0;
 }
