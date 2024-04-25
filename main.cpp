ffc0168a2d410d9b0a86c9a322b57fe870623b5e
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-25 18:56:00 +0300

    ex.2 lab had done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,17 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello world(dev)"<<endl;
+	int hryvnias, kopecks;
+	cout << "Enter hryvnias: ";
+	cin >> hryvnias;
+	cout << "Enter kopecks: ";
+	cin >> kopecks;
+
+	hryvnias += kopecks / 100;
+	kopecks %= 100;
+
+
+	cout << "You have: "<< hryvnias<<" grn and " << kopecks << " kop" << endl;
 	system("pause");
 	return 0;
 }
