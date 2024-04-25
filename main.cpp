abe13e44c313dcd6ee537c4c74a9f8b601e1ec4c
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-25 19:29:34 +0300

    ex. 5 lab had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-	cout << "Hello world(dev)"<<endl;
+	const double PI = 3.141592;
+	double radius, volume;
+
+	cout << "Calculation of the volume of a sphere\n"
+		<<"Enter the radius (cm): ";
+	cin >> radius;
+	volume = (4.0 / 3.0) * PI * pow(radius, 3);
+	cout << "Volume of the sphere: "<< volume<<" cm cubic" << endl;
 	system("pause");
 	return 0;
 }
