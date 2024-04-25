b9469d97b0c386e4d554cbdf8e760e43b70a512e
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-25 19:15:47 +0300

    ex.4 lab had done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello world(dev)"<<endl;
+	double scale, distance, res;
+	cout << "Calculation of the distance"
+		<< "between settlements.\n" 
+		<< "Enter the output data:\n"
+		<< "Map scale (number of kilometers in one centimeter) -> ";
+	cin >> scale;
+	cout << "Distance between points"
+		<<" representing settlements (cm) -> ";
+	cin >> distance;
+
+	res = scale * distance;
+	cout << "The distance between settlements is "<<res
+		<<" km."<<endl;
 	system("pause");
 	return 0;
 }
