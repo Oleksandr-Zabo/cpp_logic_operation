e72b85e1ed09c41015b7937b6a6dfe763d55bb33
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-25 19:08:57 +0300

    ex.3 lab had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
-	cout << "Hello world(dev)"<<endl;
+	double length, width, height, volume;
+	cout << "Calculation of the volume "
+		<<"of a parallelepiped." << endl
+		<<"Enter the output data:\n"
+		<<"Length (cm) -> ";
+	cin >> length;
+	cout <<"Width (cm) -> ";
+
+	cin >> width;
+	cout << "Height (cm) -> ";
+	cin >> height;
+
+	volume = length * width * height;
+
+	cout << "Volume: "<< volume <<"0 cubic cm." << endl;
 	system("pause");
 	return 0;
 }
